735e8a869160d26baa872cca2b3d0a7ff2c545a4
Anderson Ismael <55637907+aicoutos@users.noreply.github.com>
2020-02-04 16:42:22 -0300

    identificar o método

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,12 +3,13 @@ namespace App\Controller;
 use Basic\Controller;
 class Index extends Controller{
     function index(){
-        $dataArr=[
-            'title'=>'hello world'
-        ];
-        $headerStr=$this->view('inc/header',$dataArr);
-        $dataArr['header']=$headerStr;
-        $indexStr=$this->view('screen/index',$dataArr);
+        print 'hello world';
+        // $dataArr=[
+        //     'title'=>'hello world'
+        // ];
+        // $headerStr=$this->view('inc/header',$dataArr);
+        // $dataArr['header']=$headerStr;
+        // $indexStr=$this->view('screen/index',$dataArr);
     }
 }
 ?>
--- basic/Basic.php
+++ basic/Basic.php
@@ -14,5 +14,70 @@ class Basic{
             error_reporting(0);
         }
     }
+    function getInputVars(){
+        $pairs = explode("&", file_get_contents("php://input"));
+        $vars = array();
+        foreach ($pairs as $pair) {
+            $nv = explode("=", $pair);
+            $name = urldecode($nv[0]);
+            $value = urldecode($nv[1]);
+            $vars[$name] = $value;
+        }
+        return $vars;
+    }
+    function getMethod(){
+        return @$_SERVER['REQUEST_METHOD'];
+    }
+    function getUri(
+        $segmentId=null
+    ){
+        $str=$_SERVER["REQUEST_URI"];
+        $str=@explode('?',$str)[0];
+        $arr=explode('/',$str);
+        $arr=array_filter($arr);
+        $arr=array_values($arr);
+        if(count($arr)<1){
+            $segment[1]='/';
+        }else{
+            $i=1;
+            foreach ($arr as $key => $value) {
+                $segment[$i++]=$value;
+            }
+        }
+        if(is_null($segmentId)){
+            return $segment;
+        }else{
+            if(isset($segment[$segmentId])){
+                return $segment[$segmentId];
+            }else{
+                return false;
+            }
+        }
+    }
+    function run(){
+        //identificar o método
+        switch($this->getMethod()){
+            case 'GET':
+            $method=1;
+            break;
+            case 'POST':
+            $method=2;
+            break;
+            case 'PUT':
+            case 'PATCH':
+            $method=3;
+            break;
+            case 'DELETE':
+            $method=4;
+            break;
+            default:
+            $method=0;
+        }
+        print $method.'<br>';
+        //indentificar o tipo de uri
+        $uri=$this->getUri();
+        var_dump($uri);
+        //identifica a ação
+    }
 }
 ?>
