8436ef947e7ab7f5ce2fe93fd7ab8b5ecd9a64e8
Anderson Ismael <55637907+aicoutos@users.noreply.github.com>
2020-02-04 15:36:57 -0300

    function error

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-hello world
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require '../vendor/autoload.php';
+use Basic\Basic;
+$Basic=new Basic();
+$Basic->error(true);
