9bbcf42fb97e1942647c21afa3a9ba3b598a8a22
Anderson Ismael <55637907+aicoutos@users.noreply.github.com>
2020-02-06 16:23:28 -0300

    testar o dotenv

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -4,7 +4,7 @@ use Basic\Controller;
 class IndexController extends Controller{
     function index(){
         $data=[
-            'title'=>'hello world'
+            'title'=>$_ENV['SITE_NAME']
         ];
         print $this->view('inc/header',$data);
         print $this->view('screen/index',$data);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,16 @@
 <?php
 require '../vendor/autoload.php';
 use Basic\Kernel as Basic;
+use Dotenv\Dotenv;
 $Basic=new Basic();
 $Basic->error(true);
+$filename=$Basic->root().'.env';
+if(file_exists($filename)){
+    $dotenv = Dotenv::createImmutable($Basic->root());
+    $dotenv->load();
+}else{
+    die('.env not found'.PHP_EOL);
+}
+
 $Basic->autoRouting(true);
 ?>
